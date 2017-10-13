500f6b373643f4d9ed108fea5c44f8de49d9dd44
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-10-13 15:30:07 +0100

    add all files

---
+++ coursework1.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('home.html'), 200
+
+@app.route('/sub-genre')
+def subgenre():
+    return render_template('sub-genre.html'), 200
+
+@app.route('/artists')
+def art():
+    return render_template('artists.html'), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug = True)
