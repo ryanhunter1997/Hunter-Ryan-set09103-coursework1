c651af31ea38a168fd254a25fa590dec4e3011c8
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-10-19 11:00:50 +0100

    hyperlink on home working. also added next 3 sub-genres

--- coursework1.py
+++ coursework1.py
@@ -13,5 +13,9 @@ def subgenre():
 def art():
     return render_template('artists.html'), 200
 
+@app.route('/metal')
+def met():
+    return render_template('metal.html'), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
