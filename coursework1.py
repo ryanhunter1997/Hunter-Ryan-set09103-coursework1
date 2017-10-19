c80f0ea5b486b30d935396197e221014773ebbc6
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-10-19 12:45:57 +0100

    added all the bands pages for metal

--- coursework1.py
+++ coursework1.py
@@ -17,5 +17,25 @@ def art():
 def met():
     return render_template('metal.html'), 200
 
+@app.route('/ledzep')
+def led():
+    return render_template('ledzep.html'), 200
+
+@app.route('/blacksab')
+def black():
+    return render_template('blacksab.html'),200
+
+@app.route('/rage')
+def rage():
+    return render_template('rage.html'), 200
+
+@app.route('/indie')
+def ind():
+    return render_template('indie.html'), 200
+
+@app.route('/alt')
+def alt():
+    return render_template('alt.html'), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
