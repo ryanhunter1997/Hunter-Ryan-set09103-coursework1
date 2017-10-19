b0a43e0558ee53dec1d24e6c7b56b8d77be469ad
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-10-19 13:05:19 +0100

    added all bands for indie and some comments to flask app

--- coursework1.py
+++ coursework1.py
@@ -13,6 +13,7 @@ def subgenre():
 def art():
     return render_template('artists.html'), 200
 
+# all bands in metal
 @app.route('/metal')
 def met():
     return render_template('metal.html'), 200
@@ -29,10 +30,24 @@ def black():
 def rage():
     return render_template('rage.html'), 200
 
+# all bands in indie
 @app.route('/indie')
 def ind():
     return render_template('indie.html'), 200
 
+@app.route('/arctic')
+def arc():
+    return render_template('arctic.html'), 200
+
+@app.route('/radio')
+def rad():
+    return render_template('radio.html'), 200
+
+@app.route('/oasis')
+def oas():
+    return render_template('oasis.html'), 200
+
+# all bands in alt
 @app.route('/alt')
 def alt():
     return render_template('alt.html'), 200
